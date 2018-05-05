cfaa61002183b874589823e6b4449e36b5e35e6e
Vivek Bansal <vivek_bansal@bnz.co.nz>
2018-05-05 20:08:06 +1200

    what is your age

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-var ageAsString = prompt("what is your age?", "")
-    var age = Number(ageAsString);
-if( age < 40 )
-{
-    alert("oh your so young...");
-}
-else if(age == 40)
-{
-    alert("Hey,you're 40!");
-}
-else{
-    alert("Don't worry, you're young at heart");
-}
-
-alert("Thank you");
-
+// var ageAsString = prompt("what is your age?", "")
+//     var age = Number(prompt("What is your age?", ""));
+// if( age < 40 )
+// {
+//     alert("oh your so young...");
+// }
+// else if(age == 40)
+// {
+//     alert("Hey,you're 40!");
+// }
+// else{
+//     alert("Don't worry, you're young at heart");
+// }
+//
+// alert("Thank you");
+//
+var age = Number(prompt("What is your age?", ""));
